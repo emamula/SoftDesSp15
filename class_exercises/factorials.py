4aa62d5c4e1139ac9bdd5a8ce302f10fa839eea6
emamula <emily.mamula@students.olin.edu>
2015-01-29 17:04:32 -0500

    Add class_exercises/

--- class_exercises/factorials.py
+++ class_exercises/factorials.py
@@ -1,6 +1,23 @@
 def factorials(n):
+	"""
+	>>> factorials('4')
+	'24'
+	"""
 	i = 0
+	x = i
 	factorials = []
+	
+	while i < n:
+		if i == 0:
+			factorials.append(1)
+			i = i + 1
+		else:
+			factorials.append(i * x)
+			x = i * x
+			i = i + 1
 
-	while i < (n - 2):
-		factorials[i] = (i + 1)
+	return factorials
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
