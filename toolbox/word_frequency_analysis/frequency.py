5eaab93838486fd4dceaf3d54a5648696d3f45b6
Emily Mamula <emily.mamula@students.olin.edu>
2015-03-26 16:49:03 -0400

    gets word list

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -9,7 +9,25 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	words = []
+
+	f = open(file_name,'r')
+	lines = f.readlines()
+
+	curr_line = 0
+
+	while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+		curr_line += 1
+
+	lines = lines[curr_line+1:]
+
+	for line in lines:
+		words_in_line = line.split()
+
+		for word in words_in_line:
+			words.append(word)
+
+	return words
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -22,3 +40,6 @@ def get_top_n_words(word_list, n):
 				 frequently to least frequentlyoccurring
 	"""
 	pass
+
+if __name__ == "__main__":
+    get_word_list("grimm.txt")
