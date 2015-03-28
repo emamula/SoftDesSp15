a9c78e59f178556a297d39512aacd37a260ea3fc
Emily Mamula <emily.mamula@students.olin.edu>
2015-03-28 17:40:32 -0400

    submit word frequency analysis toolbox

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -11,9 +11,11 @@ def get_word_list(file_name):
 	"""
 	words = []
 
+	#open file and readf lines
 	f = open(file_name,'r')
 	lines = f.readlines()
 
+	#remove gutengerg's pre-text
 	curr_line = 0
 
 	while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
@@ -21,13 +23,19 @@ def get_word_list(file_name):
 
 	lines = lines[curr_line+1:]
 
+	#remove punctuation, split lines to get words, and cast 
+	#all words to lowercase
 	for line in lines:
+		for p in string.punctuation:
+			line = line.replace(p,"")
+
 		words_in_line = line.split()
 
 		for word in words_in_line:
-			words.append(word)
+			words.append(word.lower())
 
 	return words
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -39,7 +47,23 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	word_count = {}
+	top_n_words = []
+
+	for word in word_list:
+		if word in word_count:
+			word_count[word] += 1
+
+		if word not in word_count:
+			word_count[word] = 1
+
+	ordered_by_frequency = sorted(word_count, key = word_count.get, reverse = True)
+
+	for x in range(0,n):
+		top_n_words.append(ordered_by_frequency[x])
+
+	return top_n_words
+
 
 if __name__ == "__main__":
-    get_word_list("grimm.txt")
+    print get_top_n_words(get_word_list("grimm.txt"), 100)
